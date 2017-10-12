9f783eed810543b9997b5f6c7aa4ccbe630b9f89
kurtismash <mashkurtis+github@gmail.com>
2017-10-12 14:21:20 +0100

    Added bus departures
    Access via /bus/[location]/departures.

--- tfgmProxy.py
+++ tfgmProxy.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 #Tram departures:
 @app.route('/tram/<location>/departures.json')
-def departures(location):
+def tramDepartures(location):
 	departures = []
 	page = requests.get('%s/public-transport/tram/stops/%s-tram' %(config['url'],location))
 	soup = BeautifulSoup(page.content, 'html.parser')
@@ -61,6 +61,70 @@ def tramstops():
 		stop = stop[:-10]
 		tramstops.append(stop)
 	return jsonify(tramstops)
+
+@app.route('/bus/<location>/departures.json')
+def busDepartures(location):
+	departures = []
+	page = requests.get('https://beta.tfgm.com/public-transport/bus/stations/%s-bus' % location)
+	soup = BeautifulSoup(page.content, 'html.parser')
+	buses = soup.find_all(class_='bus')
+	if len(buses) == 0:
+		return jsonify([])
+	else:
+		buses.pop(0)
+		for bus in buses:
+			try:
+				departureDestination = bus.find(class_='departure-destination').contents[3].contents[0]
+			except:
+				departureDestination = ''
+			try:
+				departureRoute = bus.find(class_='departure-destination').contents[1].contents[0]
+			except:
+				departureRoute = ''
+			try:
+				departureOperator =  bus.find(class_='departure-operator').contents[0]
+			except:
+				departureOperator = ''
+			try:
+				departureStand = bus.find(class_='departure-stand').contents[0]
+			except:
+				departureStand = ''
+			try:
+				departureExpected = bus.find(class_='departure-expected').find(class_='figure').contents[0]
+			except:
+				departureExpected = ''
+			try:
+				departureIndicator = bus.find(class_='departure-indicator')
+				if departureIndicator.find(class_='due-icon nim-icon'):
+					departureIndicator = 'Live'
+				else:
+					departureIndicator = 'Timetabled'
+			except:
+				departureIndicator = ''
+			try:
+				palmStand = bus.find(class_='palm-stand').contents[0]
+			except:
+				palmStand = ''
+			try:
+				palmOperator = bus.find(class_='palm-operator').contents[0]
+			except:
+				palmOperator = ''
+
+
+
+
+			departures.append({
+				'departureDestination' : departureDestination,
+				'departureRoute' : departureRoute,
+				'departureOperator' : departureOperator,
+				'departureStand' : departureStand,
+				'departureExpected' : departureExpected,
+				'departureIndicator' : departureIndicator,
+				'palmStand' : palmStand,
+				'palmOperator' : palmOperator
+
+			})
+	return jsonify(departures)
 
 #Configure and serve webserver
 if __name__ == '__main__':
